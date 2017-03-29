d659447d471e42d9b24111c6e9bdb723cdf3788b
Maia <maia.rt.46@gmail.com>
2017-03-29 10:58:30 -0500

    Prueba-1-repechaje

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+window.onload=function(){
+
+	document.getElementById('registrate').addEventListener('click', function(){
+		window.location = 'registro.html' ;
+	})
+
+	document.getElementById('iniciar').addEventListener('click', function(){
+		window.location = 'iniciar.html' ;
+	})
+
+}
